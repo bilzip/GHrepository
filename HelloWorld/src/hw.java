717f4ebeb7253ea00b32f3c7b899ab39806d2406
BillZ <BillZ@BILLZ-E6430>
2019-08-14 06:53:44 -0600

    getting back into GitHub stuff...

--- HelloWorld/src/hw.java
+++ HelloWorld/src/hw.java
@@ -4,7 +4,7 @@ public class hw {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!\n");
-		System.out.println("Now running from GitHub...");
+		System.out.println("Now running from GitHub...need to explore further!");
 	}
 	
 }
