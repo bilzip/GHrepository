a5a87dafe4c982dd047a4601a49e3c057038677d
Bill Zipprich <bilzip@me.com>
2023-11-02 18:56:09 -0400

    on 11022023 made a tweak to the println statement

--- HW/src/hw.java
+++ HW/src/hw.java
@@ -3,7 +3,7 @@ public class hw {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Where is my whisky?");
+		System.out.println("Where is my whisky flask?");
 
 	}
 
